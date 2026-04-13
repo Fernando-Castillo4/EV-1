151c7a442ffc2f70ef2d0bc96383e5b03cbfd070
Fernando-Castillo4 <fern.castilloa@duocuc.cl>
2026-04-13 09:49:14 -0400

    Add files via upload

--- Fernando Castillo/GestionReportes/GestionReportes/src/main/java/com/QuickOrder/GestionReportes/model/Incidencia.java
+++ Fernando Castillo/GestionReportes/GestionReportes/src/main/java/com/QuickOrder/GestionReportes/model/Incidencia.java
@@ -2,82 +2,38 @@ package com.QuickOrder.GestionReportes.model;
 
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
+
 @Getter @Setter
 public class Incidencia {
 
+    //Datos utilizados en el codigo
     private Long id;
 
-    @NotBlank(message = "El nombre es obligatorio")
-    private String nombreCompleto;
-
-    @NotBlank(message = "La descripcion es obligatoria")
-    private String descripcion;
-
-    @NotBlank(message = "El estado es obligatorio")
-    private Estado estado;
-
-    @NotBlank(message = "El tipo del pedido es obligatorio")
-    private TipoPedidos tipoPedido;
-
-    @NotNull (message = "El monto es obligatorio")
-    private double montoTotal;
-
-    private LocalDate fechaPedido;
-
-    public void setId(){
-        this.id = id++;
+    //setter del ID
+    public void setId(Long id){
+        this.id = id;
     }
 
     public Long getId() {
         return id;
     }
 
-    public @NotBlank(message = "El nombre es obligatorio") String getNombreCompleto() {
-        return nombreCompleto;
-    }
+    private String nombreCompleto;
 
-    public void setNombreCompleto(@NotBlank(message = "El nombre es obligatorio") String nombreCompleto) {
-        this.nombreCompleto = nombreCompleto;
-    }
+    private String descripcion;
 
-    public @NotBlank(message = "La descripcion es obligatoria") String getDescripcion() {
-        return descripcion;
-    }
+    private Estado estado;
 
-    public void setDescripcion(@NotBlank(message = "La descripcion es obligatoria") String descripcion) {
-        this.descripcion = descripcion;
-    }
+    private TipoPedidos tipoPedido;
 
-    public @NotBlank(message = "El estado es obligatorio") Estado getEstado() {
-        return estado;
-    }
+    private double montoTotal;
 
-    public void setEstado(@NotBlank(message = "El estado es obligatorio") Estado estado) {
-        this.estado = estado;
-    }
 
-    public @NotBlank(message = "El tipo del pedido es obligatorio") TipoPedidos getTipoPedido() {
-        return tipoPedido;
-    }
-
-    public void setTipoPedido(@NotBlank(message = "El tipo del pedido es obligatorio") TipoPedidos tipoPedido) {
-        this.tipoPedido = tipoPedido;
-    }
-
-    public @NotNull(message = "El monto es obligatorio") double getMontoTotal() {
-        return montoTotal;
-    }
-
-    public void setMontoTotal(@NotNull(message = "El monto es obligatorio") double montoTotal) {
-        this.montoTotal = montoTotal;
-    }
+    //fecha genereada automaticamente
+    private LocalDate fechaPedido = LocalDate.now();
 
     public LocalDate getFechaPedido(){
         return fechaPedido;
--- Fernando Castillo/GestionReportes/GestionReportes/src/main/java/com/QuickOrder/GestionReportes/repository/IncidenciaRepository.java
+++ Fernando Castillo/GestionReportes/GestionReportes/src/main/java/com/QuickOrder/GestionReportes/repository/IncidenciaRepository.java
@@ -8,27 +8,37 @@ import com.QuickOrder.GestionReportes.model.Incidencia;
 
 @Repository
 public class IncidenciaRepository {
-    private ArrayList<Incidencia> listaIncidencias = new ArrayList<>();
 
+    //Lista
+    private ArrayList<Incidencia> listaIncidencias = new ArrayList<>();
+    
+    //Contador para id autoincrementable
+    private Long contador = 1L;
+
+    //funcion para obtener la lista
     public ArrayList<Incidencia> getListaIncidencias(){
         return listaIncidencias;
     }
 
-    public Incidencia getPedidosId(Long id){
+    //funcion para obtener pedidos segun su id
+    public Incidencia getPedidosId(Long i){
         for(Incidencia inc : listaIncidencias){
-            if(inc.getId().equals(id)){
+            if(i == inc.getId()){
                 return inc;
             }
         }
         return null;
     }
 
+    //funcion para id autoincremental y crear una incidencia
     public Incidencia registrarPedidos(Incidencia inc){
+        inc.setId(contador++);
         listaIncidencias.add(inc);
         return inc;
     }
 
-    public String actualizarPedido(Incidencia incidencia, Long id) {
+    //funcion para actualizar
+    public String actualizarPedido( Long id, Incidencia incidencia) {
         for(Incidencia inc : listaIncidencias){
             if(inc.getId() == id){
                 inc.setNombreCompleto(incidencia.getNombreCompleto());
@@ -43,16 +53,18 @@ public class IncidenciaRepository {
         return "No se pudieron realizar cambios";
     }
 
+    //funcion para borrar
     public String borrarPedido(Long id){
         for (Incidencia incidencia : listaIncidencias){
             if (incidencia.getId() == id){
-                listaIncidencias.remove(id);
+                listaIncidencias.remove(incidencia);
                 return "Pedido Eliminado";
             }
         }
         return "Pedido No encontrado";
     }
 
+    //funcion para obtener segun su estado
     public Incidencia getIncidenciaEstado(Incidencia inc){
         for (Incidencia incidencia : listaIncidencias){
             if (inc.getEstado() == incidencia.getEstado()){
